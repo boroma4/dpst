5ff13c766600bd354fd25dae74225b00b099234f
Bohdan <bohdan.romashchenko@gmail.com>
2021-01-12 23:24:49 +0200

    add some basic components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+        backgroundColor: theme.palette.background.default,
+        // marginTop: theme.spacing(8),
+        padding: theme.spacing(2, 0),
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        //height: '2.5rem'
+    },
+}));
+
+export default function Footer() {
+    const classes = useStyles();
+
+    return (
+        <footer className={classes.footer}>
+            <Container maxWidth="lg">
+                <Typography variant="body2" color="textSecondary" align="center">
+                    {'Copyright © '}
+                    {'Smart kids Inc.'}
+                    {new Date().getFullYear()}
+                    {'.'}
+                </Typography>
+            </Container>
+        </footer>
+    );
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import logo from '../../icon.png'
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        bar: {
+            backgroundColor: 'azure'
+        },
+        logo: {
+            '&:hover': {
+                cursor: 'pointer',
+            }
+        },
+        option: {
+            marginLeft: theme.spacing(4),
+            transition: 'all 0.50s',
+            '&:hover': {
+                cursor: 'pointer',
+                opacity: '.5'
+            }
+        },
+    }),
+);
+
+export default function Header() {
+    const classes = useStyles();
+    const history = useHistory();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" className={classes.bar}>
+                <Toolbar variant="dense">
+                    <img src={logo} height={50} width={50} alt={'logo'} className={classes.logo} onClick={()=>history.push('/')}/>
+                    <Typography variant="h6" color="primary" className={classes.option} onClick={()=>history.push('/theory')}>
+                        Theory
+                    </Typography>
+                    <Typography variant="h6" color="primary" className={classes.option} onClick={()=>history.push('/visualizer')}>
+                        Visualizer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
